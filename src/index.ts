e8a2b3ad7da96cce267116779820a403cbe61d9d
Salman Arshad <contact.salman.arshad@gmail.com>
2022-05-31 04:49:37 +0500

    WIP

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,50 @@
-import { create } from "ipfs-http-client";
-import dotenv from "dotenv";
-
-dotenv.config();
-// let neacl = require("tweetnacl");
-import nacl from "tweetnacl";
-import jsonfile from "jsonfile";
+import { create, IPFSHTTPClient } from "ipfs-http-client";
+import {SignKeyPair,sign,verify} from "tweetnacl";
 import { createHash } from "crypto";
 import fetch from "node-fetch";
 import bs58 from "bs58";
 import crypto from "crypto";
-const IMAGE_PATH = "/home/dev/soll/ipfss/image.jpeg";
-const WALLET_LOCATION = "/home/dev/soll/ipfss/image.jpeg";
-const fs = require("fs");
+
+import fs from "fs";
 
 console.log(process.env["IMAGE_PATH"]);
 
+// const IMAGE_PATH = "/home/dev/soll/ipfss/image.jpeg";
+// const WALLET_LOCATION = "/home/dev/soll/ipfss/image.jpeg";
+
+// dotenv.config();
 // const { publicKey, secretKey } = nacl.sign.keyPair.fromSecretKey(
 //   new Uint8Array(jsonfile.readFileSync("test_wallet_keypair.json"))
 // );
 
-const client = create();
+const client: IPFSHTTPClient = create();
+interface SignInterface {
+  privateKey: Uint8Array;
+  image: Buffer | Uint8Array;
+  metadata: MetaData;
+}
+interface MetaData {
+  [x: string | number | symbol]: unknown;
+}
 
-let aJsonString = JSON.stringify({
-  hello: "world2",
-});
+async function main(params: SignInterface) {
+  const { image, privateKey, metadata } = params;
+  const wallet:SignKeyPair = sign.keyPair.fromSecretKey(privateKey);
 
 
-async function main() {
-  let img = fs.readFileSync(IMAGE_PATH);
-  let files = [
+
+  const files = [
     {
       path: "metadata.json",
-      content: JSON.stringify({
-        o: 1,
-      }),
+      content: JSON.stringify(
+        metadata,
+      ),
     },
     {
       path: "image.jpeg",
-      content: img
+      content: image,
     },
   ];
-
 
   for await (const result of client.addAll(files, {
     wrapWithDirectory: true,
@@ -55,4 +59,15 @@ async function main() {
 // );
 
 // console.log(cid);
-main();
+main({
+  privateKey: new Uint8Array([
+    16, 179, 201, 59, 157, 142, 252, 32, 11, 119, 232, 101, 245, 5, 225, 239,
+    198, 225, 229, 71, 137, 216, 16, 218, 189, 7, 85, 152, 61, 139, 4, 13, 254,
+    57, 244, 142, 109, 82, 53, 187, 235, 187, 57, 34, 227, 112, 112, 124, 134,
+    154, 131, 29, 35, 170, 42, 87, 50, 12, 252, 56, 98, 253, 36, 168,
+  ]),
+  image: fs.readFileSync("../image.jpeg"),
+  metadata: {
+    name: "salman",
+  },
+});
