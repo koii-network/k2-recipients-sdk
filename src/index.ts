9f878bd8eb44e7c12795ff62ae57db7a3c8dd17f
Salman Arshad <contact.salman.arshad@gmail.com>
2022-06-01 04:34:40 +0500

    signature

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
-import { create, IPFSHTTPClient } from "ipfs-http-client";
-import {SignKeyPair,sign,verify} from "tweetnacl";
+import { create, IPFSHTTPClient, CID } from "ipfs-http-client";
+import { SignKeyPair, sign, verify } from "tweetnacl";
 import { createHash } from "crypto";
 import fetch from "node-fetch";
 import bs58 from "bs58";
 import crypto from "crypto";
 
 import fs from "fs";
+import path from "path";
+import { stringify } from "querystring";
+console.log(__dirname);
 
-console.log(process.env["IMAGE_PATH"]);
+// console.log(process.env["IMAGE_PATH"]);
 
 // const IMAGE_PATH = "/home/dev/soll/ipfss/image.jpeg";
 // const WALLET_LOCATION = "/home/dev/soll/ipfss/image.jpeg";
@@ -23,40 +26,52 @@ interface SignInterface {
   image: Buffer | Uint8Array;
   metadata: MetaData;
 }
+interface IPFSResponseItem {
+  path: string;
+  cid: CID;
+  size: number;
+}
 interface MetaData {
   [x: string | number | symbol]: unknown;
 }
 
 async function main(params: SignInterface) {
   const { image, privateKey, metadata } = params;
-  const wallet:SignKeyPair = sign.keyPair.fromSecretKey(privateKey);
+  const wallet: SignKeyPair = sign.keyPair.fromSecretKey(privateKey);
+  const publicKey = bs58.encode(wallet.publicKey);
 
-
-
-  const files = [
+  const upload = [
     {
       path: "metadata.json",
-      content: JSON.stringify(
-        metadata,
-      ),
+      content: JSON.stringify(metadata),
     },
     {
       path: "image.jpeg",
       content: image,
     },
   ];
-
-  for await (const result of client.addAll(files, {
+  const result = new Array<IPFSResponseItem>();
+  for await (const response of client.addAll(upload, {
     wrapWithDirectory: true,
   })) {
-    console.log(result);
+    console.log(response);
+    result.push(response);
   }
+
+  const basePath: string =
+    result.find((e) => {
+      e.path === "";
+    })?.path || "";
+  const cid: string = result
+    .find((e) => e.path == "image.jpeg")
+    ?.cid?.toString();
+
+  result.filter;
+  const payload:PayloadInterface = {
+    contentRegistryId: `IPFS:${basePath}:${cid}`,
+    k2PubKey: "4HV3retNdHCnNR4Q9KKdug2qQXTKvd8PJCehGJ6gTUKN",
+  };
 }
-// client.addAll
-// cid = await client.addAll(
-//   { path: "metadata.json", content: aJsonString },
-//   { wrapWithDirectory: true }
-// );
 
 // console.log(cid);
 main({
@@ -66,8 +81,14 @@ main({
     57, 244, 142, 109, 82, 53, 187, 235, 187, 57, 34, 227, 112, 112, 124, 134,
     154, 131, 29, 35, 170, 42, 87, 50, 12, 252, 56, 98, 253, 36, 168,
   ]),
-  image: fs.readFileSync("../image.jpeg"),
+  image: fs.readFileSync(path.join(__dirname, "../image.jpeg")),
   metadata: {
     name: "salman",
   },
 });
+
+interface PayloadInterface {
+  contentRegistryId:string,
+  k2PubKey: string
+}
+
